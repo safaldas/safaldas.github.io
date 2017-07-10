27a0de71d385b8bf4059d710a8ba8638497bd95a
safal <safaldasg@gmail.com>
2017-07-10 17:23:28 +0530

    added wit ai link

--- resume/app.js
+++ resume/app.js
@@ -16,11 +16,13 @@ $(function() {
         access_token: 'R56O4YAQ52JGARV5PZQIQLSZKJTEXVMP'
       },
       success: function(res) {
+        $('#answer').text('')
         console.log(res.entities)
         if (Object.keys(res.entities).length) {
           for (var obj in res.entities) {
             for (var data of res.entities[obj]) {
-              $('#answer').text(data.value)
+              $('#answer').append(data.value)
+              $('#answer').append(' ')
             }
           }
         } else {
@@ -28,6 +30,9 @@ $(function() {
             "SOrry I don't remember it, may be he didn't tell me about that. I'm angry"
           )
         }
+      },
+      error: function(){
+        $('#answer').text('Some error happened')        
       }
     })
   })
