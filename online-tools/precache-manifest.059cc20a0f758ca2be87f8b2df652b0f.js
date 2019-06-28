61a4c9656643d26dd52d1f32d3bf44ad0d05a8ca
safaldas <safaldas.g@neoito.com>
2019-06-28 12:19:16 +0530

    feat add online tools

---
+++ online-tools/precache-manifest.059cc20a0f758ca2be87f8b2df652b0f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "60a859917fe65ea1f9f070e0dc080c76",
+    "url": "./static/media/shards-dashboards-logo.60a85991.svg"
+  },
+  {
+    "revision": "73476783ca0f7583198978de7073815b",
+    "url": "./static/media/0.73476783.jpg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "95572f96163cf98a6fa3",
+    "url": "./static/js/main.95572f96.chunk.js"
+  },
+  {
+    "revision": "34825555841914eee3b6",
+    "url": "./static/js/1.34825555.chunk.js"
+  },
+  {
+    "revision": "95572f96163cf98a6fa3",
+    "url": "./static/css/main.d465effd.chunk.css"
+  },
+  {
+    "revision": "34825555841914eee3b6",
+    "url": "./static/css/1.08a63f5e.chunk.css"
+  },
+  {
+    "revision": "7a08303a92a781f9bb482120914b33ad",
+    "url": "./index.html"
+  }
+];
