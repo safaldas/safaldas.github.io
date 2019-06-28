656184322436a8ded15a0d2fb90aba640998e178
safaldas <safaldas.g@neoito.com>
2019-06-28 14:02:35 +0530

    change baseurl in packagejson

---
+++ online-tools/precache-manifest.f4bad27d7a7a70d2e2cd2c2650882f4d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "60a859917fe65ea1f9f070e0dc080c76",
+    "url": "/online-tools/static/media/shards-dashboards-logo.60a85991.svg"
+  },
+  {
+    "revision": "73476783ca0f7583198978de7073815b",
+    "url": "/online-tools/static/media/0.73476783.jpg"
+  },
+  {
+    "revision": "95593f0e81954d6e471c",
+    "url": "/online-tools/static/js/runtime~main.95593f0e.js"
+  },
+  {
+    "revision": "43fd353db88a54f5c21e",
+    "url": "/online-tools/static/js/main.43fd353d.chunk.js"
+  },
+  {
+    "revision": "34825555841914eee3b6",
+    "url": "/online-tools/static/js/1.34825555.chunk.js"
+  },
+  {
+    "revision": "43fd353db88a54f5c21e",
+    "url": "/online-tools/static/css/main.d465effd.chunk.css"
+  },
+  {
+    "revision": "34825555841914eee3b6",
+    "url": "/online-tools/static/css/1.08a63f5e.chunk.css"
+  },
+  {
+    "revision": "0f36a1509930065816244fa0a624b88a",
+    "url": "/online-tools/index.html"
+  }
+];
--- online-tools/service-worker.js
+++ online-tools/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5a9c35339b6add9b265c6da52835a175.js"
+  "/online-tools/precache-manifest.f4bad27d7a7a70d2e2cd2c2650882f4d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/online-tools/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
